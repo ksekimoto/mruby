a8ce0963f4d705533dc9554cd579c99d444bb09b
ksekimoto <jbh00623@nifty.com>
2018-05-02 13:38:04 +0900

    Saved local build configuration

---
+++ GNU_RX_elf.bat
@@ -0,0 +1,10 @@
+@Echo Off
+Set PATH=D:\dev\ide4gr-1.03\hardware\tools\gcc-rx\rx-elf\rx-\bin;%PATH%
+Set PATH=D:\dev\ide4gr-1.03\hardware\tools\gcc-rx\Other Utilities;%PATH%
+Set PATH=D:\dev\ide4gr-1.03\hardware\tools\gcc-rx\rx-elf\rx-elf\libexec\gcc\rx-elf\4.8-GNURX_v14.03;%PATH%
+Set PATH=D:\apps\GnuWin32\bin;C:\msys32\mingw32\bin;C:\msys32\usr\bin;%PATH%
+Set MAKE_MODE=unix
+
+rem cd "D:\apps\ide4gr-1.02-windows\hardware\tools\gcc-rx"
+
+Cmd "cd\" /k
